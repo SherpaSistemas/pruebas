52bab83b59fe79fbb650c91015b51bb65f63def9
Daniela <floresdan855@gmail.com>
2023-05-18 12:54:11 -0600

    Se estableción una correcta conexion en la Clase RUTAS_odbcDevart.py para la MIGRACION de Datos

--- RUTAS_odbcDevart.py
+++ RUTAS_odbcDevart.py
@@ -1,48 +1,32 @@
 import pyodbc
 
-# create a connection string
-# cnxn_str = "Driver={SQL Server};Server=localhost;Database=db_odoo;Trusted_Connection=yes;"
-cnxn_str = (
-        "Driver={Devart ODBC Driver for xBase};"
-        "DBFFormat=VisualFoxPro;"
-        "CollatingSequence=general;"
-        "UID='';"
-        "PWD='';"
-        "Database=C:/WinMAGI/DATA/;"
-    )
+# Configurar la cadena de conexión
+conn_str = 'Driver={SQL Server};Server=localhost;Database=PROD;Trusted_Connection=yes;'
 
-# conexion y cursor VFP
-cnxn = pyodbc.connect(cnxn_str)
-cursor = cnxn.cursor()
+# Establecer la conexión
+conn = pyodbc.connect(conn_str)
 
-# conexion y cursor SQL
-conn = pyodbc.connect('DSN=OdooSQL;Trusted_Connection=yes;Database=PROD;')
-cursorSQL = conn.cursor()
+# Crear cursores para las dos bases de datos
+cursor_origen = conn.cursor()  # Cursor para la base de datos de origen
+cursor_destino = conn.cursor()  # Cursor para la base de datos de destino
 
-cursor.execute("SELECT * FROM rtg") #NOMBRE DE LA TABLA EN winmagi
-rows = cursor.fetchall()[:15]
+# Consulta de selección en la tabla de origen
+cursor_origen.execute('SELECT * FROM rtg')
 
-cursorSQL.execute("TRUNCATE TABLE dbo.rutas") #NOMBRE DE LA TABLA EN mssql
-cursorSQL.commit()
+# Recuperar los datos de la tabla de origen
+datos_origen = cursor_origen.fetchall()[:5]
+cursor_origen.execute("TRUNCATE TABLE rutas")#NOMBRE DE LA TABLA EN mssql
+cursor_origen.commit()
 
-# Nombres de los campos
-columns = [column[0] for column in cursor.description]
-print(columns)
+# Insertar los datos en la tabla de destino
+for fila in datos_origen:
+    cursor_destino.execute('INSERT INTO rutas(pn,opndesc,rtgid,opnseq,opn,wc,suhr,runhr,efficiency,extdesc,resources,revin,effin,revout,effout,tooling,pursvc,[pursvcpn],pursvclt,pursvcmult,pursvcpl,pursvcpo,vendor,shipper,moldid,systemgen,runqty,labhr,flushlab,runahead,runaheadqt,coproddisc,pursvcprem,maxlogon,updatedby,updateon,labcode,wcmach,rtgctlcode,shipid,movetowc,testcode) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                           fila.pn, fila.opndesc, fila.rtgid, fila.opnseq, fila.opn, fila.wc, fila.suhr, fila.runhr, fila.efficiency, fila.extdesc, fila.resources, fila.revin, fila.effin, fila.revout, fila.effout, fila.tooling, fila.pursvc, fila.pursvcpn, fila.pursvclt, fila.pursvcmult, fila.pursvcpl, fila.pursvcpo, fila.vendor, fila.shipper, fila.moldid, fila.systemgen, fila.runqty, fila.labhr, fila.flushlab, fila.runahead, fila.runaheadqt, fila.coproddisc, fila.pursvcprem, fila.maxlogon, fila.updatedby, fila.updateon, fila.labcode, fila.wcmach, fila.rtgctlcode, fila.shipid, fila.movetowc, fila.testcode)
 
-n=1
-for row in rows:
-     print("",n,"  -------------> ",row)
-     cursorSQL.execute("INSERT INTO dbo.rutas(pn,opndesc,rtgid,opnseq,opn,wc,suhr,runhr,efficiency,extdesc,resources,revin,effin,revout,effout,tooling,pursvc,pursvcpn,pursvclt,pursvcmult,pursvcpl,pursvcpo,vendor,shipper,moldid,systemgen,runqty,labhr,flushlab,runahead,runaheadqt,coproddisc,pursvcprem,maxlogon,updatedby,updateon,labcode,wcmach,rtgctlcode,shipid,movetowc,testcode) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8], row[9], row[10],row[11],row[12],row[13],row[14],row[15],row[16],row[17],row[18],row[19], row[20], row[21], row[22],row[23],row[24],row[25],row[26],row[27],row[28],row[29],row[30], row[31], row[32], row[33],row[34],row[35],row[36],row[37],row[38],row[39],row[40],row[41])
-     # cursorSQL.execute("INSERT INTO dbo.rtg (pn) VALUES (?)",row[0])
-     n+=1
-cursorSQL.commit()
+# Confirmar los cambios en la base de datos de destino
+conn.commit()
 
-
-
-# cerrar cursor y conexion SQL
-cursorSQL.close()
+# Cerrar los cursores y la conexión
+cursor_origen.close()
+cursor_destino.close()
 conn.close()
-
-# cerrar cursor y conexion VFP
-cursor.close()
-cnxn.close()
